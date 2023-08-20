24e468cfb6e552a1fff6dfb77d6d63083f1e6776
v.rigato <valentina.rigato@ammagamma.com>
2023-08-20 18:01:39 +0200

    add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        headingColor: "#081e21",
+        smallTextColor: "#193256",
+      }
+    },
+  },
+  plugins: [],
+}
+
